0556b5303d2e6faf39a1e41a126387c8ce0454fb
Elior Abramoviz <elior@fb.com>
2023-09-06 15:05:58 +0300

    Add menu button animation

--- src/Menu.js
+++ src/Menu.js
@@ -1,13 +1,19 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 function Menu() {
   const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = useCallback(() => {
+    setIsOpen(!isOpen);
+  }, [isOpen]);
+
+  console.log('isOpen', isOpen);
+
   return (
     <div className='Menu'>
-      <div className='open-menu-button'>
-        <div className='line-1'></div>
-        <div className='line-2'></div>
-        <div className='line-3'></div>
+      <div className='open-menu-button' onClick={toggleMenu}>
+        <div className={`line-1 ${isOpen ? 'open' : ''}`}></div>
+        <div className={`line-2 ${isOpen ? 'open' : ''}`}></div>
+        <div className={`line-3 ${isOpen ? 'open' : ''}`}></div>
       </div>
     </div>
   );
