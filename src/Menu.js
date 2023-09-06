70d4167bef0cc2ceb32b5d8a1c45022141cc823a
Elior Abramoviz <elior@fb.com>
2023-09-06 14:45:06 +0300

    Add menu closed button

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,15 @@
-function Menu() {
-  return <div className='Menu'>Menu</div>;
-}
+import { useState } from 'react';
 
+function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className='Menu'>
+      <div className='open-menu-button'>
+        <div className='line-1'></div>
+        <div className='line-2'></div>
+        <div className='line-3'></div>
+      </div>
+    </div>
+  );
+}
 export default Menu;
